facb16f7ec9f1c70bae8c49eadecd4a9a0e21695
afinalstone <afinalstone@foxmail.com>
2022-04-17 15:58:14 +0800

    init

---
+++ 0101_Okhttp/src/main/java/com/example/opencode/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.opencode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+    git remote add origin git@github.com:AFinalStone/Android03_OpenCode.git
+        git branch -M main
+        git push -u origin main
